e3989ceb6d06726fe25fc7b4ea32c282e5bfa073
LilyCole <Lilia.Cole@gmail.com>
2016-09-23 15:12:57 -0700

    shows all places on homepage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,24 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+	$.ajax({
+		method: 'GET',
+		url: 'api/places',
+		success: function(data) {
+			for(var i=0; i<data.length;i++){
+				var place = data[i];
+				$('body').append('<strong>Description:</strong> '+place.description+'<br>');
+				$('body').append('<strong>Place:</strong>'+place.town+', '+place.state+', '+place.country+'<br>');
+			}
+		}
+	})
 
 });
+
+
+//   town: 'Manila',
+//   state: 'N/A',
+//   country: 'Philippines',
+//   years: 2,
+//   gps: { lat: 14.5995, long: 120.9842 },
+//   photo:
